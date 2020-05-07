8ee5202ac75f549490c643307e125abbb26b2a91
James Futhey <kidGodzilla@users.noreply.github.com>
2020-05-07 14:14:56 +0800

    Update MyExceptionHandler.java

--- src/android/MyExceptionHandler.java
+++ src/android/MyExceptionHandler.java
@@ -22,7 +22,32 @@ public class MyExceptionHandler implements
 
     public void uncaughtException(Thread thread, Throwable exception) {
 
-        Intent intent = new Intent(myActivity, myActivityClass);
+        Intent intent = myContext.getPackageManager().getLaunchIntentForPackage(myContext.getPackageName() );
+
+        intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(KEY_RESTART_INTENT, intent);
+        myContext.startActivity(intent);
+        
+        if (myContext instanceof Activity) {
+          ((Activity) myContext).finish();
+        }
+
+        Runtime.getRuntime().exit(0);
+        System.exit(2);
+            
+                  
+        /* Log.e("", "restarting app");
+        Intent restartIntent = myContext.getPackageManager()
+                .getLaunchIntentForPackage(myContext.getPackageName() );
+        PendingIntent intent = PendingIntent.getActivity(
+                myContext, 0,
+                restartIntent, Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        AlarmManager manager = (AlarmManager) myContext.getSystemService(myContext.ALARM_SERVICE);
+        manager.set(AlarmManager.RTC, System.currentTimeMillis() + 1, intent);
+        System.exit(2); */
+            
+            
+        /* Intent intent = new Intent(myActivity, myActivityClass);
         intent.putExtra("crash", true);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
                 | Intent.FLAG_ACTIVITY_CLEAR_TASK
@@ -35,6 +60,6 @@ public class MyExceptionHandler implements
 
 //        android.os.Process.killProcess(android.os.Process.myPid());
         myActivity.finish();
-        System.exit(2);
+        System.exit(2); */
     }
 }
